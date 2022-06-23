54c7de547052c87a9e7d258404182d9aa7fdabca
Jason Bakker <jasonbakker084@gmail.com>
2022-06-23 15:09:12 +0200

    Add english translation for sorting button

--- src/Gitle.Web/QueryParsers/IssueQueryParser.cs
+++ src/Gitle.Web/QueryParsers/IssueQueryParser.cs
@@ -37,15 +37,15 @@
 
         public Dictionary<string, string> AllSorts = new Dictionary<string, string>()
                             {
-                                {"CreatedBy", "Aanmelder"},
-                                {"CreatedAt", "Aanmaakdatum"},
-                                {"PickedUpBy", "Behandelaar"},
-                                {"PickedUpAt", "Behandeldatum"},
-                                {"Number", "Nummer"},
-                                {"Name", "Naam"},
-                                {"TotalHours", "Inspanning"},
-                                {"Comments.Count", "Aantal reacties"},
-                                {"IsAdministrative", "Administratief"}
+                                {"CreatedBy", "Created by"},
+                                {"CreatedAt", "Created at"},
+                                {"PickedUpBy", "Picked up by"},
+                                {"PickedUpAt", "Picked up at"},
+                                {"Number", "Number"},
+                                {"Name", "Name"},
+                                {"TotalHours", "Total hours"},
+                                {"Comments.Count", "Comments count"},
+                                {"IsAdministrative", "Administrative"}
                             };
 
 
